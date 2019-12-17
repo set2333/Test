70a175405c0ebc76246a2cbe0ecabe844f224ca2
set2333 <set2333@mail.ru>
2019-12-17 22:26:52 +0700

    Моя версия примера с API

--- app.js
+++ app.js
@@ -4,98 +4,33 @@ const fs = require('fs');
 
 const app = express();
 const jsonParser = bodyParser.json();
-
 app.use(express.static(__dirname + '/public'));
-app.get('/api/users', (req, res) => {
-    let content = fs.readFileSync('users.json', 'utf8');
-    let users = JSON.parse(content);
-    res.send(users);
+app.get('/api/getAll', (req, res) => {
+    fs.readFile('data.json', 'utf8', (err, data) => {
+        res.send(data);
+    });
 });
 
-app.get('/api/users/:id', (req, res) => {
-    let id = req.params.id;
-    let content = fs.readFileSync('users.json', 'utf8');
-    let users = JSON.parse(content);
-    let user = null;
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].id == id) {
-            user = users[i];
-            break;
-        }
+app.post('/api/addPrice', jsonParser, (req, res) => {
+    if (!req.body) {
+        res.sendStatus(400);
     }
-    if (user) {
-        res.send(user);
-    } else {
-        res.status(404).send();
-    }
-});
-
-app.post('/api/users', jsonParser, (req, res) => {
-    if (!req.body) return res.sendStatus(200);
-    let userName = req.body.name;
-    let userAge = req.body.age;
-    let user = {
-        name: userName,
-        age: userAge
-    };
-    let data = fs.readFileSync('users.json', 'utf8');
-    let users = JSON.parse(data);
-
-    let id = Math.max.apply(Math, users.map((o) => {
-        return o.id
-    }));
-    user.id = id + 1;
-    users.push(user);
-    var data = JSON.stringify(users);
-    fs.writeFileSync('users.json', data);
-    res.send(user);
-});
-
-add.delete('api/users/:id', (req, res) => {
-    let id = req.params.id;
-    let data = fs.readFileSync('users.json', 'utf8');
-    let users = JSON.parse(data);
-    let index = -1;
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].id == id) {
-            index = i;
-            break;
-        }
-    }
-    if (index > -1) {
-        let user = users.splice(index, 1)[0];
-        let data = JSON.stringify(users);
-        fs.writeFileSync('users.json', data);
-        res.send(user);
-    } else {
-        res.status(404).send();
-    }
-});
-
-app.put('/api/users/:id', jsonParser, (req, res)=>{
-    if(!req.body) return res.sendStatus(200);
-    let id = req.body.id;
-    let userName = req.body.name;
-    let userAge = req.body.age;
-    let data = fs.readFileSync('users.json', 'utf8');
-    let users = JSON.parse(data);
-    let user = null;
-    for (let i=0; i<users.length; i++) {
-        if(users[i]==id){
-            user = users[i];
-            break;
-        }
-    }
-    if(user) {
-        user.age = userAge;
-        user.name = userName;
-        let data = JSON.stringify(users);
-        fs.writeFileSync('users.json', data);
-        res.send(user);
-    }
-    else {
-        res.sendStatus(404);
-    }
+    fs.readFile('data.json', 'utf8', (err, data) => {
+        let prises = JSON.parse(data);
+        prises.push({
+            id: prises.reduce((pre, item)=> {
+                 if(item.id > pre) {
+                    return item.id;
+                }
+                return pre;
+            },0)+1,
+            name: req.body.name,
+            price: req.body.price
+        });
+        fs.writeFile('data.json', JSON.stringify(prises), ()=>{
+            res.sendStatus(200);
+        });
+    });
 });
 
 app.listen(3000);
