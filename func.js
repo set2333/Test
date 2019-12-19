7030bcc1414573f671bfea8c5a06fe1b0a0f60a7
set2333 <set2333@mail.ru>
2019-12-19 08:55:18 +0700

    Играемся с MongoDB

--- func.js
+++
@@ -1,7 +0,0 @@
-module.exports.isUndefToObj = function(obj) {
-    for (key in obj) {
-        if (obj[key] == undefined || obj[key] == null || obj[key] == "")
-            return true;
-    }
-    return false;
-}
